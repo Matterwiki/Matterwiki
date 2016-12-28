9f84ac145f7d39fe413b454f9b4959e94e967324
Nishant Arora <na.nishantarora@gmail.com>
2016-12-28 11:00:19 +0530

    Added create button and added success modal

--- client/app/static/new.jsx
+++ client/app/static/new.jsx
@@ -7,11 +7,17 @@ class NewArticle extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {body: ""};
   }
 
   handleChange() {
     this.setState({body: this.refs.body.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    $('#myModal').modal('show')
   }
 
   getRawMarkupBody() {
@@ -57,7 +63,30 @@ class NewArticle extends React.Component {
         <br/>
       <div className="row">
         <div className="col-md-12">
-          <button className="btn btn-default btn-block btn-lg" >Create Article</button>
+          <button className="btn btn-default btn-block btn-lg" onClick={this.handleSubmit}>Create Article</button>
+        </div>
+      </div>
+      <div className="modal modal-fullscreen fade" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div className="modal-body">
+              <center>
+              <div className="row">
+
+                <div className="col-md-6 col-sd-12">
+                  <h1><b>Yayyyy!</b></h1><h3>Your article has been published</h3>
+                  <br/>
+                  <br/>
+                  <button type="button" className="btn btn-default btn-block btn-lg" data-dismiss="modal">That's great</button>
+                </div>
+              </div>
+            </center>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
---
+++ client/assets/custom.js
@@ -0,0 +1,9 @@
+
+$(".modal-fullscreen").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+  }, 0);
+});
+$(".modal-fullscreen").on('hidden.bs.modal', function () {
+  $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+});
