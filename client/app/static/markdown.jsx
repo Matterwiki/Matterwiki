1b77780c3a6c38c3da64fd2175bdc1bbcb87d671
Nishant Arora <na.nishantarora@gmail.com>
2017-01-03 12:05:16 +0530

    Removed keyboard shortcuts from help

--- client/app/static/markdown.jsx
+++ client/app/static/markdown.jsx
@@ -9,45 +9,41 @@ class Markdown extends React.Component {
           <tr>
           <th>Markdown</th>
           <th>Result</th>
-          <th>Shortcut</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>**text**</td><td>Bold</td><td>Ctrl/⌘ + B</td>
+            <td>**text**</td><td>Bold</td>
           </tr>
           <tr>
-            <td>*text*</td><td>Emphasize</td><td>Ctrl/⌘ + I</td>
+            <td>*text*</td><td>Emphasize</td>
           </tr>
           <tr>
-            <td>~~text~~</td><td>Strike-through</td><td>Ctrl + Alt + U</td>
+            <td>~~text~~</td><td>Strike-through</td>
           </tr>
           <tr>
-            <td>[title](http://)</td><td>Link</td><td>Ctrl/⌘ + K</td>
+            <td>[title](http://)</td><td>Link</td>
           </tr>
           <tr>
-            <td>`code`</td><td>Inline Code</td><td>Ctrl/⌘ + Shift + K</td>
+            <td>`code`</td><td>Inline Code</td>
           </tr>
           <tr>
-            <td>![alt](http://)</td><td>Image</td><td>Ctrl/⌘ + Shift + I</td>
+            <td>![alt](http://)</td><td>Image</td>
           </tr>
           <tr>
-            <td>* item</td><td>List</td><td>Ctrl + L</td>
+            <td>* item</td><td>List</td>
           </tr>
           <tr>
-            <td>> quote</td><td>Blockquote</td><td>Ctrl + Q</td>
-          </tr>
-          <tr>
-            <td>==Highlight==</td><td>Highlight</td>
+            <td>> quote</td><td>Blockquote</td>
           </tr>
           <tr>
             <td># Heading</td><td>H1</td>
           </tr>
           <tr>
-            <td>## Heading</td><td>H2</td><td>Ctrl/⌘ + H</td>
+            <td>## Heading</td><td>H2</td>
           </tr>
           <tr>
-            <td>### Heading</td><td>H3</td><td>Ctrl/⌘ + H (x2)</td>
+            <td>### Heading</td><td>H3</td>
           </tr>
         </tbody>
       </table>
