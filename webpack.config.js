9061a6dbd03120c725e703330c953a8bd88078e9
Christian Rackerseder <git@echooff.de>
2017-01-21 08:41:44 +0100

    Yarn added
    Also upgraded Webpack to 2.2.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,29 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'client/public');
-var APP_DIR = path.resolve(__dirname, 'client/app');
+var BUILD_DIR = path.resolve(__dirname, 'client', 'public');
+var APP_DIR = path.resolve(__dirname, 'client', 'app');
 
 var config = {
-  entry: ['whatwg-fetch', APP_DIR + '/index.jsx'],
+  entry: ['whatwg-fetch', path.join(APP_DIR, 'index.jsx')],
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
   },
   module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel'
-      }
-    ]
+    rules: [{
+      test : /\.jsx?/,
+      include : APP_DIR,
+      loader : 'babel-loader'
+    }]
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-          warnings: false
+        warnings: false
       }
-      })
-    ]
+    })
+  ]
 };
-
 
 module.exports = config;
