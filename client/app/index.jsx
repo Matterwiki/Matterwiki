d935a0bceb3cfca8936dd92d1136a2677fd1abfe
krishna gopinath <hpkrishna07@gmail.com>
2017-01-22 18:13:33 -0500

    Devtool features, different environments and more
    Devtool features
    1) Enabled source maps for easier debugging of JS code.
    2) Added webpack's Hot module replacement feature, so that manual refresh isn't necessary
    
    Different environments
    Separate environments for dev and production, with each of it powered by different webpack config files

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,33 +1,28 @@
 import React from 'react';
+import { AppContainer } from 'react-hot-loader';
 import {render} from 'react-dom';
-import { Router, Route, IndexRoute, IndexRedirect, hashHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 
-import App from './components/app.jsx';
-import Home from './components/home.jsx';
-import Login from './components/login.jsx';
-import Article from './components/article.jsx';
-import NewArticle from './components/new.jsx';
-import EditArticle from './components/edit.jsx';
-import ArticleHistory from './components/history.jsx';
-import Admin from './components/admin.jsx';
-import Setup from './components/setup.jsx';
-import EditTopic from './components/edit_topics.jsx';
-import EditUser from './components/edit_users.jsx';
+import routes from './routes.jsx';
 
-render(
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Login}/>
-      <Route path="home" component={Home}/>
-      <Route path="login" component={Login}/>
-        <Route path="article/new" component={NewArticle}/>
-        <Route path="article/edit/:articleId" component={EditArticle}/>
-      <Route path="article/history/:articleId" component={ArticleHistory}/>
-      <Route path="article/:articleId" component={Article}/>
-      <Route path="admin" component={Admin}/>
-      <Route path="topic/edit/:topicId" component={EditTopic}/>
-      <Route path="user/edit/:userId" component={EditUser}/>
-      <Route path="setup" component={Setup}/>
-    </Route>
-  </Router>
-, document.getElementById('app'));
+const renderApp = (appRoutes) => {
+  render(
+    <AppContainer>
+      <Router history={hashHistory}>
+        {appRoutes()}
+      </Router>
+    </AppContainer>, 
+    document.getElementById('app')
+  )
+};
+
+
+renderApp(routes);
+
+// react HMR
+if(module.hot) {
+  module.hot.accept('./routes.jsx', () => {
+    const routeChanges = require('./routes.jsx').default;
+    renderApp(routeChanges);
+  })
+}
