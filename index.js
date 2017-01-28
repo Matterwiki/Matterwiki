52290244870336df8a220464b1a2c367d8830f90
Nishant Arora <na.nishantarora@gmail.com>
2017-01-28 11:43:29 +0530

    Added the search endpoint

--- index.js
+++ index.js
@@ -151,6 +151,9 @@ require('./api/users')(apiRoutesAdmin);
 // Importing all endpoints for archives
 require('./api/archives')(apiRoutes);
 
+// Importing the search endpoint
+require('./api/search')(apiRoutes);
+
 // Importing all endpoints which are only admin accessible
 require('./api/admin')(apiRoutesAdmin);
 
