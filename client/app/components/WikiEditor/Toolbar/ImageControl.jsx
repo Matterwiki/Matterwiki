17f5a2b750c39f82b71fc4ec290d5ed026ee41e3
krishna gopinath <hpkrishna07@gmail.com>
2017-03-07 23:29:51 -0500

    Added an ImageControl component - no functionality yet

--- client/app/components/WikiEditor/Toolbar/ImageControl.jsx
+++ client/app/components/WikiEditor/Toolbar/ImageControl.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import ToolbarButton from './ToolbarButton.jsx';
 
-const ImageControl = () => {
-	<ToolbarButton
-		key={type.label}
-		active={currentStyle.has(type.style)}
-		onToggle={toggleInlineStyle}
-		label={type.label}
-		style={type.style}
-		fa={type.fa}
-	/>
+const ImageControl = (props) => { 
+	return (
+		<div className="btn-group" role="group">
+			<ToolbarButton
+				key='image-control'
+				label='Upload image'
+				fa='fa-picture-o'
+			/>
+		</div>
+	);
 }
+
+export default ImageControl;
--- client/app/components/WikiEditor/Toolbar/index.jsx
+++ client/app/components/WikiEditor/Toolbar/index.jsx
@@ -4,12 +4,15 @@ import BlockControls from './BlockControls.jsx';
 import LevelControls from './LevelControls.jsx';
 import LinkControl from './LinkControl.jsx';
 import HistoryControls from './HistoryControls.jsx';
+import ImageControl from './ImageControl.jsx';
 
 const Toolbar = (props) => {
+	// TODO perhaps group LinkControl and InlineControl as `EntityControls`?
 	return (
 		<div className="btn-toolbar" role="toolbar">
 			<InlineControls {...props}/>
-			<LinkControl {...props}/>
+			<LinkControl {...props}/>			
+			<ImageControl {...props} />
 			<BlockControls {...props}/>
 			<LevelControls {...props}/>
 			<HistoryControls {...props}/>
