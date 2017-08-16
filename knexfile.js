6229df855dc8fe5a1e01f9bedf47409ba9c03f84
Krishna Gopinath <hpkrishna07@gmail.com>
2017-08-15 22:07:01 -0400

    cleanup knexfile, make it take it all from env variables/config.env

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,18 @@
-const env = process.env.NODE_ENV || "dev";
+const env = process.env.NODE_ENV;
 const config = require("dotenv").config({
   path: `config.${env}.env`
 });
 
 // Config probably not found!
-// TODO Customize this error
 if (config.error) {
   throw new Error(config.error);
 }
 
 const connection = {
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST,
   user: process.env.DB_USER_NAME,
   password: process.env.DB_PASSWORD,
-  // make env specific database
-  database: `${process.env.DB_NAME}${env}`,
+  database: process.env.DB_NAME,
   debug: !!process.env.DB_DEBUGINFO
 };
 
