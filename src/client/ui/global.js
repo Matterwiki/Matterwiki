5671cf576b85d482df8bcaae6a617a2157081d89
Nishant Arora <na.nishantarora@gmail.com>
2017-12-22 21:50:42 +0000

    Better font rendering

--- src/client/ui/global.js
+++ src/client/ui/global.js
@@ -4,6 +4,8 @@ import { injectGlobal } from "styled-components";
 injectGlobal`
   body {
     padding-top: 6rem;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
   :focus {outline:none;}
   ::-moz-focus-inner {border:0;}
