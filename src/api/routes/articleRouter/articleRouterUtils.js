118988d3ed5173f6ede182cade28377f5c670e99
Krishna Gopinath <hpkrishna07@gmail.com>
2018-01-20 15:54:02 -0500

    Fix an update error for articles

--- src/api/routes/articleRouter/articleRouterUtils.js
+++ src/api/routes/articleRouter/articleRouterUtils.js
@@ -1,12 +1,22 @@
-const { has, isEmpty } = require("lodash");
+const { has, isEmpty, isNumber } = require("lodash");
 
 const validateArticle = ({ title, content, topic_id }) => !!title && !!content && !!topic_id;
 
-const validateArticleDuringUpdate = body =>
-  ["title", "content", "topic_id", "change_log"].reduce((acc, f) => {
+function validateArticleDuringUpdate(body) {
+  if (!body) return false;
+
+  // Special treatment for integers
+  const isValidTopicId = isNumber(body.topic_id);
+
+  // For strings..
+  const allFieldsValid = ["title", "content", "change_log"].reduce((acc, f) => {
+    // Valid if body has that field and is not empty
     if (has(body, f)) return acc && !isEmpty(body[f]);
 
     return acc;
-  }, true);
+  }, isValidTopicId);
+
+  return allFieldsValid;
+}
 
 module.exports = { validateArticle, validateArticleDuringUpdate };
