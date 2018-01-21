f4485b8abb195c3a14f49349ec8d5e92601fdb5c
Nishant Arora <na.nishantarora@gmail.com>
2018-01-21 11:36:08 +0000

    color fix

--- src/client/ui/blockquote.js
+++ src/client/ui/blockquote.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Blockquote = styled.blockquote`
-  border-left: 0.3rem solid $color-quaternary;
+  border-left: 0.3rem solid ${props => (props.theme ? props.theme.secondaryBackground : "#efefef")};
   margin-left: 0;
   margin-right: 0;
   padding: 1rem 1.5rem;
--- src/client/ui/divider.js
+++ src/client/ui/divider.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Divider = styled.hr`
   border: 0;
-  border-top: 0.1rem solid $color-tertiary;
+  border-top: 0.1rem solid ${props => (props.theme ? props.theme.border : "#d1d1d1")};
   margin: 3rem 0;
 `;
 
