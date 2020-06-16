424a59f4856bd13b44af62b8e9be67de80ca45e1
Krishna Gopinath <hpkrishna07@gmail.com>
2020-06-15 20:12:51 -0400

    refactor: use dotenv-cli instead of using 'dotenv' in code

--- packages/api/knexfile.js
+++ packages/api/knexfile.js
@@ -1,5 +1,3 @@
-require('dotenv').config()
-
 const { knexSnakeCaseMappers } = require('objection')
 
 module.exports = {
--- packages/api/src/server.js
+++ packages/api/src/server.js
@@ -1,5 +1,3 @@
-require('dotenv').config()
-
 const app = require('./app')
 const { logger, validateEnvVars } = require('./common/utils/index')
 
