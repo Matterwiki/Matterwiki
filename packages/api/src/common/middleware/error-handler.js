b393b9109eaac8bc09387f619dcb92752fbe2c02
Krishna Gopinath <hpkrishna07@gmail.com>
2020-09-13 14:31:33 -0400

    chore: more upgrades

--- packages/api/src/common/middleware/error-handler.js
+++ packages/api/src/common/middleware/error-handler.js
@@ -1,3 +1,5 @@
+const HttpStatusCodes = require('http-status-codes')
+
 module.exports = function globalErrorHandler(err, req, res, next) {
     const { status, message } = err
     if (
@@ -5,14 +7,16 @@ module.exports = function globalErrorHandler(err, req, res, next) {
         process.env.NODE_ENV === 'test'
     ) {
         // Development error handler that prints stacktrace
-        return res.status(status || 500).json({
-            message: message,
-            error: err,
-        })
+        return res
+            .status(status || HttpStatusCodes.INTERNAL_SERVER_ERROR)
+            .json({
+                message: message,
+                error: err,
+            })
     }
 
     // Production error handler that does not leak stacktraces to user
-    res.status(err.status || 500).json({
+    res.status(err.status || HttpStatusCodes.INTERNAL_SERVER_ERROR).json({
         message: message,
         error: {},
     })
